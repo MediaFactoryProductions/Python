9d78fcbbc9fd13fcc00f1ca026f504bb9be28097
MediaFactoryProductions <mediafactoryprod@gmail.com>
2023-10-30 22:42:28 -0500

    Variable Project Commit

---
+++ Variables/variables.py
@@ -0,0 +1,23 @@
+#variabale assignment
+
+# our way of communicating information to the compiler
+
+y = "Hello, World"
+print(y)
+
+x = 5
+z = 3.14
+c = 10
+d = 2
+
+a = x + 10
+b = z * 2
+
+print(a)
+print(b)
+
+is_true = True
+is_false = False
+
+print(is_true)
+print(is_false)
