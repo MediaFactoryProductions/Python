4ca28c66db01ed314d16762d706cd0721e5ba775
MediaFactoryProductions <mediafactoryprod@gmail.com>
2023-11-07 14:59:08 -0600

    Commit

---
+++ String Indexing/String_Indexing.py
@@ -0,0 +1,11 @@
+text = "Hello, World!"
+
+first_char = text[0]
+fifth_char = text[4]
+last_char = text[-1] 
+substring = text[7:12]
+
+print("First character:", first_char)
+print("Fifth character:", fifth_char)
+print("Last character:", last_char)
+print("Substring:", substring)
